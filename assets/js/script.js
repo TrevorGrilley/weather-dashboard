8e58f55c237f099f7633c8f289d17ea3bbf503a8
Trevor <trevorgrilley@yahoo.com>
2022-11-15 22:54:31 -0500

    create vars and input api key

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var inputEl = document.getElementById("city-search");
+var searchEl = document.getElementById("search-button");
+var clearEl = document.getElementById("clear-previous-searches");
+var nameEl = document.getElementById("city-name");
+var currentPicEl = document.getElementById("current-pic");
+var currentTempEl = document.getElementById("temperature");
+var currentHumidityEl = document.getElementById("humidity");4
+var currentWindEl = document.getElementById("wind-speed");
+var currentUVEl = document.getElementById("UV-index");
+var historyEl = document.getElementById("history"); 
+var cityStoredList = [];
+
+
+var apiKey = "0c960b0d0237228b3b34cbe765e682dd";
